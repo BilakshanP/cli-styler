f4f0f25a17ffabc596af85c51ea128cd567369a2
BilakshanP <pbilakshan@gmail.com>
2026-02-17 14:45:17 +0530

    Update: Improved feature flags

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,10 +2,3 @@
 fn main() {
     cli_styler::cli::wrapped_run();
 }
-
-#[cfg(not(feature = "cli"))]
-fn main() {
-    eprintln!(
-        "Error: 'cli' feature not enabled, pass the \"--features=cli\" flag during compilation."
-    );
-}
